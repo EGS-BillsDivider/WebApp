97cf23e87e15b147c45373e9cfa1d0169df8e578
Pedro Abreu <pedro.abreu@ua.pt>
2022-04-03 20:08:02 +0100

    x

--- app/src/App.js
+++ app/src/App.js
@@ -29,84 +29,111 @@ export default App;
 
 
 function Main() {
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/bill', {
+      method: 'POST',
+      headers:  {'Content-Type': 'application/json'},
+      body: JSON.stringify(inputs)
+    })
+    .then(response => response.json())
+    .then(inputs => {
+      console.log('Success:', inputs);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+  
+  //-------------------------------------
+
+  const [show, setShow] = useState(false);
     
-    const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Mysidebar></Mysidebar>
+      <Navbar id="Navbar" bg="light">
+          <Container fluid>
+          <Navbar.Brand href="#">Maison Aveiro</Navbar.Brand>
+          </Container>
+      </Navbar>
+      <div class="container">
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>titulo da conta</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">descrição</Card.Subtitle>
+            <Card.Text>
+              Tiago 7,68 euros
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
       
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-      <>
-        <Mysidebar></Mysidebar>
-        <Navbar id="Navbar" bg="light">
-            <Container fluid>
-            <Navbar.Brand href="#">Maison Aveiro</Navbar.Brand>
-            </Container>
-        </Navbar>
-        <div class="container">
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>titulo da conta</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">descrição</Card.Subtitle>
-              <Card.Text>
-                Tiago 7,68 euros
-              </Card.Text>
-            </Card.Body>
-          </Card>
+      <div class="container">
+        <div class="d-flex justify-content-end">
+          <Button variant="primary" onClick={handleShow}>
+            Add Bill
+          </Button>
         </div>
-        
-        <div class="container">
-          <div class="d-flex justify-content-end">
-            <Button variant="primary" onClick={handleShow}>
-              Add Bill
-            </Button>
-          </div>
-        </div>
-    
-        <Modal show={show} onHide={handleClose} centered size="lg">
-          <Modal.Header closeButton>
-              <Modal.Title >Adicionar nova conta</Modal.Title>
-          </Modal.Header>
-          <Form>
-            <Modal.Body>
-              <Form.Group className="mb-3">
-                <Form.Label>Título</Form.Label>
-                <Form.Control type="text" placeholder="ex: Luz Nov. Tiago" />
-              </Form.Group>
+      </div>
+  
+      <Modal show={show} onHide={handleClose} centered size="lg" onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+            <Modal.Title >Adicionar nova conta</Modal.Title>
+        </Modal.Header>
+        <Form>
+          <Modal.Body>
+            <Form.Group className="mb-3">
+              <Form.Label>Título</Form.Label>
+              <Form.Control type="text" placeholder="ex: Luz Nov. Tiago" name="titulo" value={inputs.titulo || ""} onChange={handleChange}/>
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Descrição</Form.Label>
-                <Form.Control as="textarea" rows={3} />
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Descrição</Form.Label>
+              <Form.Control as="textarea" rows={3} name="descricao" value={inputs.descricao || ""} onChange={handleChange}/>
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Total</Form.Label>
-                  <NumericInput step={0.01} placeholder="ex: 32.56"/>
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Total</Form.Label>
+              <Form.Control type="number" name="amount" value={inputs.amount || ""} onChange={handleChange}/>
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <div class="d-flex row">
-                  <div class="col">
-                    <Form.Label>Fatura</Form.Label>
-                    <Form.Control type="file" size="sm"/>
-                  </div>
-                  <div class="col">
-                    <Form.Label>Recibo</Form.Label>
-                    <Form.Control type="file" size="sm"/>
-                  </div>
+            <Form.Group className="mb-3">
+              <div class="d-flex row">
+                <div class="col">
+                  <Form.Label>Fatura</Form.Label>
+                  <Form.Control type="file" size="sm" name="fatura" value={inputs.fatura || ""} onChange={handleChange}/>
                 </div>
-              </Form.Group>
+                <div class="col">
+                  <Form.Label>Recibo</Form.Label>
+                  <Form.Control type="file" size="sm" name="recibo" value={inputs.recibo || ""} onChange={handleChange}/>
+                </div>
+              </div>
+            </Form.Group>
 
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" onClick={handleClose} type="submit">
-                  Adicionar
-                </Button>
-            </Modal.Footer>
-          </Form>
-        </Modal>
-        
-        </>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="primary" onClick={handleClose} type="submit">
+                Adicionar
+              </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+      
+      </>
     );
   }
 
